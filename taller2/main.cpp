2a41ed8acb0ce8ea032f1b406016cb49ecc1b78e
juan pablo sanchez <juansanchezg@javeriana.edu.co>
2022-02-23 13:47:57 -0500

    menu del taller 3 de analisis y creando main para el putno 4 del taller 2

--- taller2/main.cpp
+++ taller2/main.cpp
@@ -26,83 +26,107 @@ int main()
 {
     censo persona[5000];
     llenarCenso(persona, 5000);
-    return 0;
-}
-
-void llenarCenso(censo persona[], int n)
-{
-    char strrnd[10];
-    srand(time(NULL));
-
-    for (int i = 0; i < n; i++)
+    do
     {
-        for (int i = 0; i <= 5; i++)
-        {
-            strrnd[i] = 33 + rand() % (126 - 33);
-        }
-        string tmp_string(strrnd);
-        persona[i].nombre = tmp_string;
-        persona[i].edad = rand() % (99 - 18 + 1) + 18;
-        persona[i].numero = llenarNumeroPersona(persona, i);
+        cout << "1. Busqueda por nombre" << endl;
+        cout << "2. Busqueda por numero" << endl;
+        cout << "3. Salir" << endl;
+        int opcion;
+        cin >> opcion;
+        string nombre;
+        string numero;
 
-        if (persona[i].edad < 65)
+        switch (opcion)
         {
+        case 1:
+            cout << "Ingrese nombre: ";
+            cin >> nombre;
+            busquedaPorNombre(persona, 5000, nombre);
+            break;
+        case 2:
+            cout << "Ingrese numero: ";
+            cin >> numero;
+            busquedaPorNumero(persona, 5000, numero);
+            break;
+        case 3:
+            break;
+        }
+    }
+
+    void llenarCenso(censo persona[], int n)
+    {
+        char strrnd[10];
+        srand(time(NULL));
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                strrnd[i] = 33 + rand() % (126 - 33);
+            }
+            string tmp_string(strrnd);
+            persona[i].nombre = tmp_string;
+            persona[i].edad = rand() % (99 - 18 + 1) + 18;
+            persona[i].numero = llenarNumeroPersona(persona, i);
+
+            if (persona[i].edad < 65)
+            {
+                persona[i].impuestos = true;
+            }
+            else
+            {
+                persona[i].impuestos = false;
+            }
             persona[i].impuestos = true;
         }
-        else
+    }
+    string llenarNumeroPersona(censo persona[], int pos)
+    {
+        int num[10];
+        srand(time(NULL));
+        for (int i = 0; i <= 9; i++)
         {
-            persona[i].impuestos = false;
+            num[i] = 0 + rand() % (9);
         }
-        persona[i].impuestos = true;
-    }
-}
-string llenarNumeroPersona(censo persona[], int pos)
-{
-    int num[10];
-    srand(time(NULL));
-    for (int i = 0; i <= 9; i++)
-    {
-        num[i] = 0 + rand() % (9);
-    }
-    string str;
-    for (int i : num)
-    {
-        str.push_back(i + '0');
-    }
-    cout << str << endl;
-    if (pos == 0)
-    {
-        return str;
-    }
-    else
-    {
-        if (verificarRepetidos(persona, 5000, str))
+        string str;
+        for (int i : num)
         {
-            return llenarNumeroPersona(persona, pos);
+            str.push_back(i + '0');
         }
-        else
+        cout << str << endl;
+        if (pos == 0)
         {
             return str;
         }
-    }
-}
-
-bool verificarRepetidos(censo persona[], int tam, string numero)
-{
-    for (int i = 0; i < tam; i++)
-    {
-        if (persona[i].numero == numero)
+        else
         {
-            return true;
+            if (verificarRepetidos(persona, 5000, str))
+            {
+                return llenarNumeroPersona(persona, pos);
+            }
+            else
+            {
+                return str;
+            }
         }
     }
-    return false;
-}
 
-void busquedaPorNombre(censo persona[], int tam, string nombre)
-{
-}
+    bool verificarRepetidos(censo persona[], int tam, string numero)
+    {
+        for (int i = 0; i < tam; i++)
+        {
+            if (persona[i].numero == numero)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
-void busquedaPorNumero(censo persona[], int tam, string numero)
-{
-}
+    void busquedaPorNombre(censo persona[], int tam, string nombre)
+    {
+    }
+
+    void busquedaPorNumero(censo persona[], int tam, string numero)
+    {
+    }
